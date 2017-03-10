b9e61a989863a4670c50c62e1dbd898a28d30ba0
Sergio Gutierrez <sergio@gokarumi.com>
2017-03-10 23:21:55 +0100

    Print map from file. Fix several issues

--- src/pandemic/view/video_buffer.clj
+++ src/pandemic/view/video_buffer.clj
@@ -10,7 +10,13 @@
   "Returns a newly created video buffer"
   {:terminal (terminal/get-terminal :text {:cols buffer-width :rows buffer-height})
    :drawille (drawille/->canvas (* 2 buffer-width) (* 4 buffer-height))
-   :buffer (vec (repeat buffer-height (str/join (repeat buffer-width " "))))
+   :buffers {:white (vec (repeat buffer-height (str/join (repeat buffer-width " "))))
+             :red (vec (repeat buffer-height (str/join (repeat buffer-width " "))))
+             :blue (vec (repeat buffer-height (str/join (repeat buffer-width " "))))
+             :cyan (vec (repeat buffer-height (str/join (repeat buffer-width " "))))
+             :green (vec (repeat buffer-height (str/join (repeat buffer-width " "))))
+             :yellow (vec (repeat buffer-height (str/join (repeat buffer-width " "))))
+             :default (vec (repeat buffer-height (str/join (repeat buffer-width " "))))}
    :canvas-x 0
    :canvas-y 0
    :canvas-width canvas-width
@@ -30,23 +36,24 @@
     (str/join (assoc (vec string) i char))))
 
 (defn put-char
-  [video-buffer char x y]
+  [video-buffer char x y & {:keys [color]
+                            :or {color :white}}]
   (assoc-in
    video-buffer
-   [:buffer y]
-   (put-char-in-string (get (:buffer video-buffer) y) char x)))
+   [:buffers color y]
+   (put-char-in-string (get (color (:buffers video-buffer)) y) char x)))
 
 (defn put-string
-  [video-buffer string x y  & {:keys [color]
-                               :or {color :white}}]
+  [video-buffer string x y & {:keys [color]
+                              :or {color :white}}]
   "Puts the given string in the buffer at position (x, y)
   Whitespaces are treated as non-changing characters"
-  (let [row (get (:buffer video-buffer) y)
+  (let [row (get (color (:buffers video-buffer)) y)
         min-col x
         max-col (dec (min (count row) (+ (count string) min-col)))]
     (assoc-in
      video-buffer
-     [:buffer y]
+     [:buffers color y]
      (str/join
       (map-indexed
        (fn [index item]
@@ -58,7 +65,8 @@
        row)))))
 
 (defn put-strings
-  [video-buffer strings x y]
+  [video-buffer strings x y & {:keys [color]
+                               :or {color :white}}]
   "Put a list of strings in the given coordinates as if they occupy a rectangle"
   (loop [video-buffer video-buffer
          strings strings
@@ -67,7 +75,7 @@
      (or (= row (:buffer-height video-buffer))
          (empty? strings))
       video-buffer
-      (recur (put-string video-buffer (first strings) x row)
+      (recur (put-string video-buffer (first strings) x row :color color)
              (vec (drop 1 strings))
              (inc row)))))
 
@@ -91,17 +99,27 @@
                    (+ dy (:canvas-y video-buffer)))
               (- (:buffer-height video-buffer) (:canvas-height video-buffer))))))
 
-(defn flush-buffer
-  [video-buffer]
-  "Prints the contents of the buffer in the terminal"
+(defn- flush-color-buffer
+  [video-buffer color]
   (let [min-row (:canvas-y video-buffer)
         max-row (+ min-row (:canvas-height video-buffer))
         indexed-rows (->>
-                      (map-indexed vector (:buffer video-buffer))
+                      (map-indexed vector (color (:buffers video-buffer)))
                       (filter (fn [[index _]] (<= min-row index max-row))))]
+    (terminal/set-fg-color (:terminal video-buffer) color)
     (doseq [indexed-row indexed-rows]
       (let [min-col (:canvas-x video-buffer)
             max-col (+ min-col (:canvas-width video-buffer))
             row (first indexed-row)
             string (subs (second indexed-row) min-col max-col)]
-        (terminal/put-string (:terminal video-buffer) string 0 (- row min-row))))))
+        (doseq [[x char] (map-indexed vector string)]
+          (when (not= char \space)
+            (terminal/put-string (:terminal video-buffer) (str char) x (- row min-row))))))))
+
+(defn flush-buffer
+  [video-buffer]
+  "Prints the contents of the buffer in the terminal"
+  (doseq [row (range (:buffer-height video-buffer))]
+    (terminal/put-string (:terminal video-buffer) (str/join (repeat (:buffer-width video-buffer) " ")) 0 row))
+  (doseq [color [:white :cyan :red :blue :green :yellow :default]]
+    (flush-color-buffer video-buffer color)))
